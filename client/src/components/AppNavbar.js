2a79d4acd124fe828c893bba59935323d9728839
animesh <animeshshah94@gmail.com>
2024-03-20 17:41:19 +0530

    update

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -23,7 +23,7 @@ class AppNavbar extends Component {
     render() {
         return (
             <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Navbar color="danger" dark expand="sm" className="mb-5">
                     <Container>
                         <NavbarBrand href="/">ShoppingList</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
