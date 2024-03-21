c31ffa4d05e69e29180603f7b69a6cae94c8e2e2
animesh <animeshshah94@gmail.com>
2024-03-21 15:13:17 +0530

    update1

--- server.js
+++ server.js
@@ -25,15 +25,25 @@ mongoose
 // Use Routes
 app.use('/api/items', items);
 
-//Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    //Set static folder
-    app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));  
-    });
-}
+//Serve static assets if in production
+// if(process.env.NODE_ENV === 'production') {
+//     //Set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));  
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 
